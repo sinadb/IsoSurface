1fd6d6c66fc3dd30564075a0403289573296b624
Sina <sinadashtebozorgy@gmail.com>
2024-03-07 19:23:40 +0000

    Initial Commit

---
+++ surface/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  surface
+//
+//  Created by Sina Dashtebozorgy on 07/03/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
